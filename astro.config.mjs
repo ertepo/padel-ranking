e773b81a9ec49862ce785f746e6ad4d883a29b39
ertepo <ertepo@gmail.com>
2026-05-16 11:38:17 +0200

    setup netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import netlify from '@astrojs/netlify';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'server',
+
+  adapter: netlify(),
+
   integrations: [svelte()],
+
   vite: {
     plugins: [tailwindcss()],
+  },
+
+  devToolbar: {
+    enabled: false,
   },
 });
